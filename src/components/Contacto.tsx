c77f71c57e4bff7832cbf3aa950445a4dace20fe
leinerhc <leinerdavidhc20@gmail.com>
2023-12-14 21:40:19 -0500

    emailer  added  and deploy

--- src/components/Contacto.tsx
+++ src/components/Contacto.tsx
@@ -1,5 +1,39 @@
+import type React from "react";
 import { className } from "./Button";
 export default function Contacto() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    const myForm = event.currentTarget;
+    const formData = new FormData(myForm);
+  
+    // Convertir FormData a objeto compatible con URLSearchParams
+    const formDataObject: Record<string, string> = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value as string;
+    });
+  
+    // Crear URLSearchParams a partir del objeto
+    const searchParams = new URLSearchParams(formDataObject);
+  
+    try {
+      const response = await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: searchParams.toString(),
+      });
+  
+      if (response.ok) {
+        alert("Thank you for your submission");
+      } else {
+        throw new Error("Submission failed");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+  
+  
   return (
     <section
       className="mt-10 mx-4 flex flex-col justify-center items-center"
@@ -42,26 +76,57 @@ export default function Contacto() {
                 </a>
               </li>
               <li className="flex items-center justify-center w-max hover:text-secondary transition-all duration-300">
-                <a href="mailto:leinerdavidhc20@gmail.com" className="flex justify-center items-center gap-1" target="_blank">
-                  <i className="bx bxs-envelope text-2xl"></i> Leinerdavidhc20@gmail.com
+                <a
+                  href="mailto:leinerdavidhc20@gmail.com"
+                  className="flex justify-center items-center gap-1"
+                  target="_blank"
+                >
+                  <i className="bx bxs-envelope text-2xl"></i>{" "}
+                  Leinerdavidhc20@gmail.com
                 </a>
               </li>
             </ul>
           </article>
-          <form className="flex flex-col gap-4 md:w-1/2 w-full p-3 items-center">
+          <form
+            className="flex flex-col gap-4 md:w-1/2 w-full p-3 items-center"
+            onSubmit={handleSubmit}
+          >
             <div className="bg-primary border border-primary rounded-md shadow-md shadow-primary lg:w-[90%] w-full">
-              <input type="text" placeholder="Ingresa tu nombre" name="nombre" className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"/>
+              <input
+                type="text"
+                placeholder="Ingresa tu nombre"
+                name="nombre"
+                className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"
+              />
             </div>
             <div className="bg-primary border border-primary rounded-md shadow-md shadow-primary lg:w-[90%] w-full">
-              <input type="email" placeholder="Ingresa tu email" name="email" className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"/>
+              <input
+                type="email"
+                placeholder="Ingresa tu email"
+                name="email"
+                className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"
+              />
             </div>
             <div className="bg-primary border border-primary rounded-md shadow-md shadow-primary lg:w-[90%] w-full">
-              <input type="text" placeholder="¿Sobre qué quieres hablar?" name="asunto" className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"/>
+              <input
+                type="text"
+                placeholder="¿Sobre qué quieres hablar?"
+                name="asunto"
+                className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"
+              />
             </div>
             <div className="bg-primary border border-primary rounded-md shadow-md shadow-primary lg:w-[90%] w-full">
-            <textarea placeholder="Escribe tu mensaje" name="mensaje" className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary" rows={10} cols={30}></textarea>
+              <textarea
+                placeholder="Escribe tu mensaje"
+                name="mensaje"
+                className="bg-background border border-primary rounded-md w-full p-2 outline-none focus:translate-x-[0.4em] focus:translate-y-[-0.4em] active:translate-x-[0.4em] active:translate-y-[-0.4em] placeholder-no-show transition-all duration-300 placeholder:text-secondary text-secondary"
+                rows={10}
+                cols={30}
+              ></textarea>
             </div>
-            <button className={className}>Enviar Mensaje</button>
+            <button className={className} type="submit">
+              Enviar Mensaje
+            </button>
           </form>
         </div>
       </article>
