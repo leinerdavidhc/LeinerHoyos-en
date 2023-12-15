965771153d3eaea37a09af0623a146d54c805b02
leinerhc <leinerdavidhc20@gmail.com>
2023-12-15 14:27:06 -0500

    page proyectos  added  and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es", "pt-br"]
+  },
 });
